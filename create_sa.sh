0259a86d2857e16519a09aa19945f063e2138655
Pierre Mavro <pmavro@qovery.com>
2023-04-22 16:25:57 +0200

    fix: args issue

--- create_sa.sh
+++ create_sa.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+JOB_ACTION=$1
+
 echo "-> Ensuring required environment variables are present"
 all_required_env_vars=(SERVICE_ACCOUNT_NAME QOVERY_KUBERNETES_NAMESPACE_NAME QOVERY_KUBERNETES_CLUSTER_NAME QOVERY_CLOUD_PROVIDER_REGION AWS_ROLE_ARN)
 for env_var in ${all_required_env_vars[@]}; do
@@ -30,17 +32,17 @@ echo -e "-> Generated service account:\n$(cat sa.yaml)"
 echo "-> Getting kubeconfig"
 aws eks update-kubeconfig --region $QOVERY_CLOUD_PROVIDER_REGION --name $QOVERY_KUBERNETES_CLUSTER_NAME
 
-case $1 in
-    start)
+case "$JOB_ACTION" in
+    "start")
         echo "-> Deploying service account"
         kubectl apply -f sa.yaml
         ;;
-    delete)
+    "delete")
         echo "-> Deleting service account"
         kubectl delete -f sa.yaml
         ;;
     *)
-        echo "-> Unssuported job action: '$1'. Only 'start' and 'delete' are supported"
+        echo "-> Unssuported job action: '$JOB_ACTION'. Only 'start' and 'delete' are supported"
         echo 'Please check the tutorial: https://hub.qovery.com/guides/tutorial/use-aws-iam-roles-with-qovery/#create-a-lifecycle-job'
         exit 1
         ;;
